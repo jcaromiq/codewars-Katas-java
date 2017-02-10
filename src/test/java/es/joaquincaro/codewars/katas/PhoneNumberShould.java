0869a4728bf68596191e73e2981355c6feebf32b
Joaquín Caro <me@joaquin-caro.es>
2017-02-10 22:55:53 +0100

    Test for "PhoneNumber" jata

---
+++ src/test/java/es/joaquincaro/codewars/katas/PhoneNumberShould.java
@@ -0,0 +1,16 @@
+package es.joaquincaro.codewars.katas;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class PhoneNumberShould {
+	
+	@Test
+	public void generate_phone_number_given_ten_digits() throws Exception {
+		
+		assertThat(true, equalTo(true));
+	}
+
+}
