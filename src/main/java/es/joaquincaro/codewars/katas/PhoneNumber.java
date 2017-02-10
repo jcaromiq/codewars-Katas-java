108178cfb8a233dd70a03defd8a023c30e765cf1
Joaquín Caro <me@joaquin-caro.es>
2017-02-10 23:27:57 +0100

    Refactor Create blocks of numbers

--- src/main/java/es/joaquincaro/codewars/katas/PhoneNumber.java
+++ src/main/java/es/joaquincaro/codewars/katas/PhoneNumber.java
@@ -1,14 +1,29 @@
 package es.joaquincaro.codewars.katas;
 
+import java.util.stream.IntStream;
+
 public class PhoneNumber {
 
 
 	public static String createPhoneNumber(int[] numbers) {
-		String phone = "";
+		StringBuffer prefix = new StringBuffer("");
+		
+		IntStream.range(0, 3).forEach(index ->{
+			prefix.append(numbers[index]);
+		});
+		
+		StringBuffer middle = new StringBuffer("");
+		IntStream.range(3, 6).forEach(index ->{
+			middle.append(numbers[index]);
+		});
+		
+		StringBuffer last = new StringBuffer("");
+		IntStream.range(6, 10).forEach(index ->{
+			last.append(numbers[index]);
+		});
 
-		phone = "(" + numbers[0] + numbers[1] + numbers[2] + ") "+ numbers[3]+numbers[4]+numbers[5]+"-"+numbers[6]+numbers[7]+numbers[8]+ numbers[9];  
+		return String.format("(%s) %s-%s", prefix, middle, last);
 				
-		return phone;
 	}
 
 }
